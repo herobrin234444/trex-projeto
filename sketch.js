81f3e2527eff64def1138521e5b28401f58b8454
herobrin234444 <93288140+herobrin234444@users.noreply.github.com>
2021-11-17 17:01:04 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,13 +4,17 @@ var estadodejogo = JOGAR;
 var grupodenuvens;
 var grupodeobstaculos;
 var solo2;
-var trex, trex_correndo, edges;
+var trex, trex_correndo, edges, trex_colidido;
 var solo, imagen_do_solo;
 var nuvens, nuvens2;
 var obstaculo, obstaculo1, obstaculo2, obstaculo3, obstaculo4, obstaculo5, obstaculo6;
 var score;
-function preload(){
+var gameover, restart,gameoverpng,restartpng;
 
+function preload(){
+  restartpng = loadImage("restart.png")
+  gameoverpng = loadImage("gameOver.png");
+  trex_colidido = loadAnimation ("trex_collided.png");
   trex_correndo = loadAnimation("trex1.png", "trex3.png", "trex4.png");
   imagensolo = loadImage("ground2.png");
   nuvens2 = loadImage("cloud.png");
@@ -29,8 +33,12 @@ function setup(){
   grupodeobstaculos = new Group();
 
   trex = createSprite(30,160,20,50);
-  trex.addAnimation("running", trex_correndo); 
+  trex.addAnimation("running", trex_correndo);
+  trex.addAnimation("colisão",trex_colidido);
   trex.scale = 0.5;
+  trex.setCollider("circle",0,0,40);
+  trex.debug =  true
+  
 
   solo = createSprite(300,180,600,20);
  
@@ -42,18 +50,30 @@ function setup(){
 
   var rand = Math.round(random(10,60));
 
+  gameover = createSprite(300,100);
+  gameover.addImage("fim_de_jogo",gameoverpng);
+  gameover.scale = 0.50
+  
+  restart = createSprite(300,140);
+  restart.addImage("recomeçar",restartpng);
+  restart.scale = 0.50
+
+
+
   score = 0;
 }
 
 function draw(){
-  background(83);
+  background(255);
 
   text("SCORE:" +score,500,50);
-
+  console.log("isto é"+estadodejogo)
 
   if (estadodejogo === JOGAR){
     solo.velocityX = -4;
 
+    gameover.visible = false;
+    restart.visible = false;
     score=score+Math.round(frameCount/120);
 
     if(keyDown("space") && trex.y >160){
@@ -71,10 +91,19 @@ function draw(){
      if (grupodeobstaculos.isTouching(trex)){
        estadodejogo = ENCERRAR;
      }
+
   }
   else if (estadodejogo === ENCERRAR){
     solo.velocityX = 0;
 
+    gameover.visible = true;
+    restart.visible = true;
+
+    trex.velocityY = 0
+
+    trex.changeAnimation("colisão",trex_colidido);
+    grupodenuvens.setLifetimeEach(-1);
+    grupodeobstaculos.setLifetimeEach(-1);
     grupodenuvens.setVelocityXEach (0);
     grupodeobstaculos.setVelocityXEach (0);
   }
